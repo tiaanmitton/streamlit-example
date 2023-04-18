42d49a4cedd81d7429b9d96d35dad4ca13bc2e4e
tiaanmitton <129163468+tiaanmitton@users.noreply.github.com>
2023-04-18 16:24:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,4 @@ routes = pd.read_csv('routes.dat', header=None)
 route_col = ['Airline', 'Airline ID', 'Source airport', 'Source airport ID', 'Destination airport', 'Destination airport ID', 'Codeshare', 'Stops', 'Equipment']
 routes.columns = route_col
 
-
+st.table(airports)
