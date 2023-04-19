03bea83590dab73c157c3274d3f443fcfe2ff09d
tiaanmitton <129163468+tiaanmitton@users.noreply.github.com>
2023-04-19 14:00:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 
-def local_css(file_name):
-    with open(file_name) as f:
+def local_css('style.css'):
+    with open('style.css') as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 local_css('style.css')
