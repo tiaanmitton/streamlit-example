334cd918c59eaff21f8fa25acd040f6b4a37d925
tiaanmitton <129163468+tiaanmitton@users.noreply.github.com>
2023-04-19 11:12:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,44 @@ routes.columns = route_col
 
 st.write("My table")
 st.table(airports)
+
+
+import folium
+from folium.plugins import MeasureControl
+from ipywidgets import interact, Dropdown
+
+m = folium.Map(location=[37.7749, -122.4194], zoom_start=3)
+
+# Create a dropdown menu for selecting the start airport
+start_airport_dropdown = Dropdown(options=airports['Name'].tolist(), description='Start airport:')
+
+# Create a dropdown menu for selecting the end airport
+end_airport_dropdown = Dropdown(options=airports['Name'].tolist(), description='End airport:')
+
+# Function to update the map based on the selected airports
+def update_map(start_airport, end_airport):
+    # Get the latitude and longitude of the start airport
+    start_airport_data = airports.loc[airports['Name'] == start_airport]
+    start_latitude = start_airport_data['Latitude'].values[0]
+    start_longitude = start_airport_data['Longitude'].values[0]
+    
+    # Get the latitude and longitude of the end airport
+    end_airport_data = airports.loc[airports['Name'] == end_airport]
+    end_latitude = end_airport_data['Latitude'].values[0]
+    end_longitude = end_airport_data['Longitude'].values[0]
+    
+    # Add a polyline between the start and end airports
+    folium.PolyLine(locations=[[start_latitude, start_longitude], [end_latitude, end_longitude]], color='red').add_to(m)
+
+# Add the dropdown menus to the map
+start_airport_dropdown.observe(lambda change: update_map(change.new, end_airport_dropdown.value), names='value')
+end_airport_dropdown.observe(lambda change: update_map(start_airport_dropdown.value, change.new), names='value')
+
+# Add a scale control to the map
+m.add_child(MeasureControl())
+
+# Display the map and dropdown menus
+display(start_airport_dropdown)
+display(end_airport_dropdown)
+display(m)
+
