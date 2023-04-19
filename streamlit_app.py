5254b4ebfad22fb1fef958e7a7a80eac4087b9b3
tiaanmitton <129163468+tiaanmitton@users.noreply.github.com>
2023-04-19 14:02:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,18 @@
 
-def local_css('style.css'):
-    with open('style.css') as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-local_css('style.css')
-
-
 import pandas as pd
 import folium
 from folium.plugins import MeasureControl
 import streamlit as st
 from streamlit_folium import folium_static
 from math import radians, cos, sin, asin, sqrt
+
+import streamlit as st
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+local_css('style.css')
 
 
 
